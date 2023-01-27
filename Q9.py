f92f39d4934f43d6d2de4f91cb8120e94ac36e49
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2023-01-27 23:14:52 +0530

    Update Q9.py

--- Q9.py
+++ Q9.py
@@ -5,12 +5,18 @@ class isValid:
     def Valid(self):
         isValid = True
         for k in range(len(self.inString)):
+            if not inValid:
+                break
             c = self.inString[k]
             if c in[")","}","]"]:###if an open bracket is placed then check for previous
                 if k == 0:###First position then isn't valid
                     isValid = False
                 if self.inString[k-1] not in ["(","{","["]:###prev position wasn't brakets
                     isValid = False
+               ###if ( or any other at the last of string then also invalid
+            if c in ["(","{","["] and k == len(self.inString)-1:
+                inValid = False
+                break
             if c == "(" :
                 if self.inString[k+1] != ")":
                          isValid = False
